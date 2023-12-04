5b3cf1d3eb8330e790480a719b08f919b6984d41
Matheus Dutra <matheusdutra0207@gmail.com>
2023-12-04 13:24:45 -0300

    adding launch directory in packge

--- odrive_ros2_pkg/setup.py
+++ odrive_ros2_pkg/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from os import path
+from glob import glob
 
 package_name = 'odrive_ros2_pkg'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (path.join('share', package_name, 'launch'), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools',
                       "pyserial==3.5",
