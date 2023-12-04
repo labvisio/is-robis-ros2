e3e33ed063535d2a4d77e8024bea65685f1f32c5
Matheus Dutra <matheusdutra0207@gmail.com>
2023-12-04 14:29:14 -0300

    adding params file to setup.py

--- odrive_ros2_pkg/setup.py
+++ odrive_ros2_pkg/setup.py
@@ -2,31 +2,32 @@ from setuptools import find_packages, setup
 from os import path
 from glob import glob
 
-package_name = 'odrive_ros2_pkg'
+package_name = "odrive_ros2_pkg"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (path.join('share', package_name, 'launch'), glob('launch/*launch.[pxy][yma]*'))
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            path.join("share", package_name, "launch"),
+            glob("launch/*launch.[pxy][yma]*"),
+        ),
+        (
+            path.join("share", package_name, "params"),
+            glob("params/*.yaml"),
+        ),
     ],
-    install_requires=['setuptools',
-                      "pyserial==3.5",
-                      'transforms3d==0.4.1'
-                    ],
+    install_requires=["setuptools", "pyserial==3.5", "transforms3d==0.4.1"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="root",
+    maintainer_email="root@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'odrive_node = odrive_ros2_pkg.odrive_node:main'
-        ],
+        "console_scripts": ["odrive_node = odrive_ros2_pkg.odrive_node:main"],
     },
 )
